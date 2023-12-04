a51ef1ba8e688d0c19cda45ca6b2383fb89f3901
WsbModzel <133588913+WsbModzel@users.noreply.github.com>
2023-12-04 15:49:39 +0100

    files with comments

--- login.js
+++ login.js
@@ -1,17 +1,20 @@
-commandline
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const loginForm = document.getElementById("login-form"); //przypisanie do zmiennej elementu login-form
+const loginButton = document.getElementById("login-form-submit"); //przypisanie do zmiennej elementu login-form-submit
+const loginErrorMsg = document.getElementById("login-error-msg"); //przypisanie do zmiennej elementu login-error-msg
 
-loginButton.addEventListener("click", (e) => {
+loginButton.addEventListener("click", (e) => { //odpalanie funkcji po wciśnieciu przycisku login
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+    const username = loginForm.username.value; //przypisanie wartosci username z formularza do zmiennej
+    const password = loginForm.password.value; //przypisanie wartosci password z formularza do zmiennej
 
-    if (username === "" && password === "") {
+    //porownanie czy wartosci username i password sa puste, jesli tak wyskoczy powiadomienie o udanym logowaniu. Jesli nie, zmieni sie wartosci wiadomosci bledu
+    //i pojawi sie informacje o bledzie
+    
+    if (username === "" && password === "") { 
         alert("You have successfully logged in.");
         location.reload();
     } else {
-        loginErrorMsg.style.opacity = 1;
+        alert('Username or password incorrect. Please try again.');
+               
     }
 })
